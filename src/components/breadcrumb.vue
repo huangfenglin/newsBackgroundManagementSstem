75eefd4d319eaeb69c0145babd95285cd0fbc58d
huangfenglin <effort0755@163.com>
2019-11-04 17:42:16 +0800

    面包屑导航

---
+++ src/components/breadcrumb.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="breadcrumb">{{breadcrumb}}</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    breadcrumb() {
+      let res = '';
+      this.$route.matched.forEach( e => {
+        res += e.meta + '->';
+      })
+      return res
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.breadcrumb {
+    padding: 10px 0;
+}
+</style>
